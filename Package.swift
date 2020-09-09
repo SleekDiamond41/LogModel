56ef359aac20b346bdbb3953956394c72839eace
Michael Arrington <michael@clevercoding.com>
2020-09-08 21:47:49 -0600

    Updated Swift Package to hopefully allow for profiling down the line

--- Package.swift
+++ Package.swift
@@ -5,7 +5,11 @@ import PackageDescription
 
 let package = Package(
     name: "LogModel",
-    products: [
+	platforms: [
+		.macOS(.v10_12),
+		.iOS(.v13),
+	],
+	products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "LogModel",
