7fbb2c3b338ec008fcfdfbfa017d23063fc63949
Michael Arrington <michael@clevercoding.com>
2020-09-08 21:47:25 -0600

    Removed concept of 'customData' and added method for logging assertions

--- Sources/LogModel/Log.swift
+++ Sources/LogModel/Log.swift
@@ -146,8 +146,32 @@ extension Log {
 extension Log {
 	
 	
+	/// Logs an error and raises an assertion failure if `condition` resolves to false; otherwise has no effect.
+	///
+	/// This method may be used in place of `Swift.assert(_:)` and `Swift.assert(_:,_:)`.
+	/// Note that the standard `assert` methods have no effect in a production application,
+	/// but this method will log failures regardless of the target environment (at virtually no performance cost).
+	///
+	/// - Parameters:
+	///   - condition: a condition which must be met in order for the program to operate correctly
+	///   - message: an error message to display in the event of an unmet `condition`
+	public func assert(_ condition: @autoclosure () -> Bool, _ message: Message, file: StaticString = #file, function: String = #function, line: UInt = #line) {
+		
+		let didPass = condition()
+		
+		if didPass {
+			// nothing negative to report
+			return
+		}
+		
+		error(message, file: file.description, function: function, line: UInt32(line))
+		
+		Swift.assertionFailure(message.description, file: file, line: line)
+	}
+	
+	
 	/// Extra information that might be useful to have, such as entering or exiting low-level functions.
-	public func verbose(_ message: Message, customData: String? = nil, file: String = #file, function: String = #function, line: UInt32 = #line) {
+	public func verbose(_ message: Message, file: String = #file, function: String = #function, line: UInt32 = #line) {
 		
 		let threadID = Thread.current.hashValue
 		
@@ -167,7 +191,7 @@ extension Log {
 	
 	
 	/// Debugging information. These messages should ideally describe exactly what happened that was incorrect, possible reasons this state may have been entered, and possible solutions to implement that might fix this failure.
-	public func debug(_ message: Message, customData: String? = nil, file: String = #file, function: String = #function, line: UInt32 = #line) {
+	public func debug(_ message: Message, file: String = #file, function: String = #function, line: UInt32 = #line) {
 		let threadID = Thread.current.hashValue
 		
 		let data = EntryData(date: Date(),
@@ -185,7 +209,7 @@ extension Log {
 	}
 	
 	/// General information, such as user interactions
-	public func info(_ message: Message, customData: String? = nil, file: String = #file, function: String = #function, line: UInt32 = #line) {
+	public func info(_ message: Message, file: String = #file, function: String = #function, line: UInt32 = #line) {
 		let threadID = Thread.current.hashValue
 		
 		let data = EntryData(date: Date(),
@@ -203,7 +227,7 @@ extension Log {
 	}
 	
 	/// Entered an unfortunate, but recoverable state.
-	public func warning(_ message: Message, customData: String? = nil, file: String = #file, function: String = #function, line: UInt32 = #line) {
+	public func warning(_ message: Message, file: String = #file, function: String = #function, line: UInt32 = #line) {
 		let threadID = Thread.current.hashValue
 		
 		let data = EntryData(date: Date(),
@@ -221,7 +245,7 @@ extension Log {
 	}
 	
 	/// Entered an unrecoverable state.
-	public func error(_ message: Message, customData: String? = nil, file: String = #file, function: String = #function, line: UInt32 = #line) {
+	public func error(_ message: Message, file: String = #file, function: String = #function, line: UInt32 = #line) {
 		let threadID = Thread.current.hashValue
 		
 		let data = EntryData(date: Date(),
@@ -247,7 +271,7 @@ extension Log {
 	///         log.wtf("we _just_ set `a` to 0, but it was nil during the next instruction")
 	///         preconditionFailure()
 	///     }
-	public func wtf(_ message: Message, customData: String? = nil, file: String = #file, function: String = #function, line: UInt32 = #line) {
+	public func wtf(_ message: Message, file: String = #file, function: String = #function, line: UInt32 = #line) {
 		let threadID = Thread.current.hashValue
 		
 		let data = EntryData(date: Date(),
